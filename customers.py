d3e99757c57dedda057f8198fc55957baea0b8e9
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2022-05-27 11:49:53 +0300

    updatedcustomerfile  with oop

--- customers.py
+++ customers.py
@@ -8,46 +8,43 @@ try:
     customer_data = open('customers.json')
     customers = json.load(customer_data)
 except Exception as e:
-    print(e)
-    customers=[] 
-    
+        print(e)
+        customers=[] 
+        print(customers)
 
 class Customer():
+    def __init__(self, user_id=None, name=None, phonenumber=None, address=None):
+        self.user_id=user_id
+        self.name=name
+        self.phonenumber=phonenumber
+        self.address=address
+    
+    def __str__(self):
+        return ( f'{self.user_id} {self.name} {self.phonenumber} {self.address}')
 
-    def __init__(self, data):
-        self.data = data
+def get_all_customers():
+        print(customers)
 
-    def save_customer(self):
+def save_customer(data):
 
         new_customer = {}
         new_customer['user_id'] = str(uuid4())
-        new_customer["name"] = self.data['name']
-        new_customer["phonenumber"] = self.data['phonenumber']
-        new_customer["address"] = self.data['address']
+        new_customer["name"] = data['name']
+        new_customer["phonenumber"] = data['phonenumber']
+        new_customer["address"] = data['address']
+        
+        
+        customer = Customer(new_customer['user_id'], new_customer['name'], new_customer['phonenumber'], new_customer['address'])
+        get_all_customers()
+
 
         customers.append(new_customer)
 
         with open('customers.json', 'w') as file_out:
             json.dump(customers, file_out, indent=0)
 
-def get_all_customers():
-    #breakpoint()
-    print(customers)
-    
         
-        
-
-
-        
-
-        
-
-        #print("so far so good")
-        
-
 def add_customer():
-
-    
 
     continue_add = True
     while continue_add:
@@ -61,24 +58,17 @@ def add_customer():
             "address":address
             
         }
-        addedcustomer= Customer(data)
-        addedcustomer.save_customer()
-        print(customers)
+        save_customer(data)
         choice=int(input("enter 1 to add another customer or 2 to exit: "))
         if choice==2:
             continue_add=False
             print("Customer (s) added")
 
 
-
-           
-        
-
-
-
-
 def update_customer():
-    get_all_customers()#calling this function to be able to see the id's
+    customer = Customer()
+    customers = customer.get_all_customers()
+    #calling the above function to be able to see the ids
     yourid=input("enter id: ")
     for cust in customers:
         #breakpoint()
@@ -100,7 +90,9 @@ def update_customer():
 
 
 def delete_customer():
-    get_all_customers()
+    customer = Customer()
+    customers = customer.get_all_customers()
+    #get_all_customers()
     yourid=input("enter customer id: ")
 
 
@@ -160,4 +152,5 @@ def customer_menu():
      
 
         
+
 
