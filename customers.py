d80a2303c76eccfbe6e9a6ad2cd34739aa7e299b
EuniceKiingati <47145776+EuniceKiingati@users.noreply.github.com>
2022-06-05 14:40:02 +0300

    updated

--- customers.py
+++ customers.py
@@ -2,113 +2,112 @@
 
 import pdb
 import json
-from uuid import uuid4
+import random
 
 try:
     customer_data = open('customers.json')
     customers = json.load(customer_data)
 except Exception as e:
-        print(e)
-        customers=[] 
-        print(customers)
+    print(e)
+    customers = []
+    print(customers)
+
 
 class Customer():
     def __init__(self, user_id=None, name=None, phonenumber=None, address=None):
-        self.user_id=user_id
-        self.name=name
-        self.phonenumber=phonenumber
-        self.address=address
-    
+        self.user_id = user_id
+        self.name = name
+        self.phonenumber = phonenumber
+        self.address = address
+
     def __str__(self):
-        return ( f'{self.user_id} {self.name} {self.phonenumber} {self.address}')
+        return (f'{self.user_id} {self.name} {self.phonenumber} {self.address}')
+
 
 def get_all_customers():
-        print(customers)
+    print(customers)
+    return customers
+
 
 def save_customer(data):
 
-        new_customer = {}
-        new_customer['user_id'] = str(uuid4())
-        new_customer["name"] = data['name']
-        new_customer["phonenumber"] = data['phonenumber']
-        new_customer["address"] = data['address']
-        
-        
-        customer = Customer(new_customer['user_id'], new_customer['name'], new_customer['phonenumber'], new_customer['address'])
-        get_all_customers()
+    new_customer = {}
+    new_customer['user_id'] = random.randint(1000, 10000)
+    new_customer["name"] = data['name']
+    new_customer["phonenumber"] = data['phonenumber']
+    new_customer["address"] = data['address']
+
+    customer = Customer(new_customer['user_id'], new_customer['name'],
+                        new_customer['phonenumber'], new_customer['address'])
+    get_all_customers()
+
+    customers.append(new_customer)
+
+    with open('customers.json', 'w') as file_out:
+        json.dump(customers, file_out, indent=0)
 
 
-        customers.append(new_customer)
-
-        with open('customers.json', 'w') as file_out:
-            json.dump(customers, file_out, indent=0)
-
-        
 def add_customer():
 
     continue_add = True
     while continue_add:
 
-        name =input("Enter name: ")
-        phonenumber=int(input("enter phonenumber: "))
-        address=str(input("enter address: "))
+        name = input("Enter name: ")
+        phonenumber = int(input("enter phonenumber: "))
+        address = str(input("enter address: "))
         data = {
             "name": name,
-            "phonenumber":phonenumber,
-            "address":address
-            
+            "phonenumber": phonenumber,
+            "address": address
+
         }
         save_customer(data)
-        choice=int(input("enter 1 to add another customer or 2 to exit: "))
-        if choice==2:
-            continue_add=False
+        choice = int(input("enter 1 to add another customer or 2 to exit: "))
+        if choice == 2:
+            continue_add = False
             print("Customer (s) added")
 
 
 def update_customer():
-    customer = Customer()
-    customers = customer.get_all_customers()
-    #calling the above function to be able to see the ids
-    yourid=input("enter id: ")
+    get_all_customers()
+    # calling the above function to be able to see the ids
+    yourid = input("enter id: ")
     for cust in customers:
-        #breakpoint()
+        # breakpoint()
 
-        if cust['user_id']== yourid:
+        if cust['user_id'] == yourid:
 
-            
-            
-            newnumber=int(input("Enter new number: "))
-             
-            newaddress=str(input("Enter new address: "))
-            cust['phonenumber']=newnumber
-            cust['address']=newaddress
-            #breakpoint()
+            newnumber = int(input("Enter new number: "))
+
+            newaddress = str(input("Enter new address: "))
+            cust['phonenumber'] = newnumber
+            cust['address'] = newaddress
+            # breakpoint()
 
     with open('customers.json', 'w') as file_out:
-            json.dump(customers, file_out, indent=0)
-            print("Customer updated successfully")
+        json.dump(customers, file_out, indent=0)
+        print("Customer updated successfully")
 
 
 def delete_customer():
-    customer = Customer()
-    customers = customer.get_all_customers()
-    #get_all_customers()
-    yourid=input("enter customer id: ")
+    get_all_customers()
+    continue_delete=True
+    while continue_delete:
+    # get_all_customers()
+        yourid = input("enter customer id: ")
 
-
-    for i in range(len(customers)):
-        if customers[i]['user_id']==yourid:
-            customers.remove(customers[i])
+        for i in range(len(customers)):
+            if customers[i]['user_id'] == yourid:
+                customers.remove(customers[i])
             break
-    #breakpoint()
-    with open('customers.json', 'w') as file_out:
-            json.dump(customers, file_out, indent=0)
-            print("Customer deleted successfully")
-
-           
-        
-
-
+    # breakpoint()
+        with open('customers.json', 'w') as file_out:
+            json.dump(customers, file_out, indent=2)
+            #print("Customer deleted successfully")
+        choice = int(input("enter 1 to delete another customer or 2 to exit: "))
+        if choice == 2:
+            continue_delete = False
+            print("Customer (s) deleted")
 
 
 def customer_menu():
@@ -117,40 +116,16 @@ def customer_menu():
     print("2:update customer")
     print("3:Delete customer")
 
-    selection=int(input("enter your choice: "))
-    if selection==1:
+    selection = int(input("enter your choice: "))
+    if selection == 1:
         add_customer()
-    elif selection==2:
+    elif selection == 2:
         update_customer()
-    elif selection==3:
+    elif selection == 3:
         delete_customer()
 
+        # print(cust)
 
-        
-       
-        
-            #print(cust)
-
-
-    
-
-
-
-
-
-
-
-
-        
 
 # import pdb;
 # pdb.set_trace()
-        
-
-#with open('customers.json', 'w') as file_out:
-    #json.dump(customers, file_out, indent=0)
-     
-
-        
-
-
--- products.py
+++ products.py
@@ -2,126 +2,108 @@
 
 import pdb
 import json
-from uuid import uuid4
+import random
 
 try:
     product_data = open('products.json')
     products = json.load(product_data)
 except Exception as e:
     print(e)
-    products=[] 
-    
+    products = []
+
 
 class Product():
 
-     def __init__(self, product_id=None, name=None, size=None, price=None):
-        self.product_id=product_id
-        self.name=name
-        self.size=size
-        self.price=price
+    def __init__(self, product_id=None, name=None, quantity=None, price=None):
+        self.product_id = product_id
+        self.name = name
+        self.quantity = quantity
+        self.price = price
 
-     def __str__(self):
-        return ( f'{self.product_id} {self.name} {self.size} {self.price}')
-    
+    def __str__(self):
+        return (f'{self.product_id} {self.name} {self.quantity} {self.price}')
+
 
 def get_all_products():
-    #breakpoint()
     print(products)
+    return products
+
 
 def save_product(data):
-        new_product = {}
-        new_product['product_id'] = str(uuid4())
-        new_product["name"] = data['name']
-        new_product["size"] = data['size']
-        new_product["price"] =data['price']
-        product=Product(new_product['product_id'], new_product['name'],new_product['size'],new_product['price'])
+    new_product = {}
+    new_product['product_id'] = random.randint(20000, 90000)
+    new_product["name"] = data['name']
+    new_product["quantity"] = data['quantity']
+    new_product["price"] = data['price']
+    product = Product(new_product['product_id'], new_product['name'],
+                      new_product['quantity'], new_product['price'])
 
-        get_all_products()
-        products.append(new_product)
+    get_all_products()
+    products.append(new_product)
 
-        with open('products.json', 'w') as pfile_out:
-            json.dump(products, pfile_out, indent=0)
-    
-        
-        
+    with open('products.json', 'w') as pfile_out:
+        json.dump(products, pfile_out, indent=2)
 
+    #print("so far so good")
 
-        
-
-        
-
-        #print("so far so good")
-        
 
 def add_product():
-
-    
 
     continue_add = True
     while continue_add:
 
-        name =input("Enter name: ")
-        size=str(input("enter size: "))
-        price=int(input("enter price: "))
+        name = input("Enter name: ")
+        quantity = int(input("enter quantity: "))
+        price = int(input("enter price: "))
         data = {
             "name": name,
-            "size":size,
-            "price":price
-            
+            "quantity": quantity,
+            "price": price
+
         }
         save_product(data)
         print(products)
-        choice=int(input("enter 1 to add another product or 2 to exit: "))
-        if choice==2:
-            continue_add=False
+        choice = int(input("enter 1 to add another product or 2 to exit: "))
+        if choice == 2:
+            continue_add = False
             print("Product (s) added")
 
 
+def edit_product(data):
 
-           
-        
-
-
+    with open('products.json', 'w') as pfile_out:
+        json.dump(data, pfile_out, indent=2)
+        print("Product updated successfully")
 
 
 def update_product():
-    get_all_products()#calling this function to be able to see the id's
-    prodid=input("enter product-id: ")
+    get_all_products()  # calling this function to be able to see the id's
+    prodid = input("enter product-id: ")
     for prod in products:
-        #breakpoint()
+        # breakpoint()
 
-        if prod['product_id']== prodid:
+        if prod['product_id'] == prodid:
 
-            
-            
-            newprice=int(input("Enter new price: "))
-             
-            prod['price']=newprice
-            #breakpoint()
+            newprice = int(input("Enter new price: "))
 
-    with open('products.json', 'w') as pfile_out:
-            json.dump(products, pfile_out, indent=0)
-            print("Product updated successfully")
+            prod['price'] = newprice
+            # breakpoint()
+
+    edit_product(products)
 
 
 def delete_product():
     get_all_products()
-    prodid=input("enter customer id: ")
-
+    prodid = input("enter customer id: ")
 
     for i in range(len(products)):
-        if products[i]['product_id']==prodid:
+        if products[i]['product_id'] == prodid:
             products.remove(products[i])
             break
-    #breakpoint()
+    # breakpoint()
     with open('products.json', 'w') as pfile_out:
-            json.dump(products, pfile_out, indent=0)
-            print("Product deleted successfully")
-
-           
-        
-
-
+        json.dump(products, pfile_out, indent=2)
+        print("Product deleted successfully")
 
 
 def product_menu():
@@ -130,41 +112,22 @@ def product_menu():
     print("2:update product")
     print("3:Delete product")
 
-    selection=int(input("enter your choice: "))
-    if selection==1:
+    selection = int(input("enter your choice: "))
+    if selection == 1:
         add_product()
-    elif selection==2:
+    elif selection == 2:
         update_product()
-    elif selection==3:
+    elif selection == 3:
         delete_product()
 
+        # print(cust)
 
-        
-       
-        
-            #print(cust)
-
-
-    
-
-
-
-
-
-
-
-
-        
 
 # import pdb;
 # pdb.set_trace()
-        
 
-#with open('customers.json', 'w') as file_out:
-    #json.dump(customers, file_out, indent=0)
-     
 
-        
-
+# with open('customers.json', 'w') as file_out:
+    #json.dump(customers, file_out, indent=2)
 
     #print("working on it")
